c08032a3913e9ad7ebaeb3a2fc5c691ed51b4d77
RENATO SILVA DE OLIVEIRA <26210937@dsn127m1246734.cfp127aluno.local>
2026-09-25 16:04:15 -0300

    criei a central do codigo, while true

--- lista_de_tarefa.py
+++ lista_de_tarefa.py
@@ -13,3 +13,14 @@
 # {"titulo: "ler", "concluida: "nao","prioridade:"baixa"}
 #]
 
+tarefas = ["matematica", "portugues","ciencias","progrmacao","ingles"]
+
+while True:
+    print("--->Veja suas tarefas<---")
+    print("1- todas as tarefas")
+    print("2- tarefas concluidas")
+    print("3- tarefas pendentes")
+    print("4- tarefas de prioridade")
+    print("5-cadastrar nova tarefa")
+    print("finalizar tarefa")
+    print("0- sair")
